05d5a09027e7e32e6aaeb1e0b9f0e2506ad6de6f
matheus-sousa-nascimento <143366856+matheus-sousa-nascimento@users.noreply.github.com>
2025-03-17 23:52:11 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercicio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            double num = double.Parse(Console.ReadLine());
+            double sucessor = num + 1;
+            double antecessor = num - 1;
+            Console.WriteLine("O número digitado foi " + num + ", seu antecessor é " + antecessor +" e seu sucessor é " + sucessor);
+        }
+    }
+}
